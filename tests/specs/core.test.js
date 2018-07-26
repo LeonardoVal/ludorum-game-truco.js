c37e4e6849c9ba9d401bded4fbe2a4c903a9380e
LeonardoVal <leonardo.val@creatartis.com>
2018-07-25 21:36:25 -0300

    First test case + Stupidly simple. Only to avoid Karma throwing an error.

---
+++ tests/specs/core.test.js
@@ -0,0 +1,13 @@
+﻿// This is a copy fo similar test cases used in ludorum.
+define(['creatartis-base', 'sermat', 'ludorum', 'ludorum-game-truco'],
+function (base, Sermat, ludorum, ludorum_game_truco) {
+	
+	describe("ludorum-game-truco", function () {
+		
+		it("core definitions", function () {
+			expect(typeof ludorum_game_truco.Truco).toBe('function');
+		}); //// it "core definitions"
+
+	}); //// describe "ludorum-game-truco"
+
+}); //// define.
