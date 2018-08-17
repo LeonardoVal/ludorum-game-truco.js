650cd390483111b62fe693f4da281bae950f9967
Lucas Lois <lucasloisp@gmail.com>
2018-08-17 12:01:39 -0300

    Tests move generation

--- tests/specs/core.test.js
+++ tests/specs/core.test.js
@@ -15,9 +15,16 @@ function (base, Sermat, ludorum, ludorum_game_truco) {
 		it("calculates possible moves", function () {
 			var game = new ludorum_game_truco.ai.SubTruco("Hand", [2, 4, 6], [1, 3, 5]);
 
-			expect(game.moves()[game.activePlayer()]).toEqual([0, 1, 2]);
-			game.activePlayer = "Foot";
-			expect(game.moves()[game.activePlayer()]).toEqual([0, 1, 2]);
+			expect(game.moves().Hand).toEqual([0, 1, 2]);
+			console.log(game.activePlayer());
+			// Switch to foot
+			//expect(game.moves().Foot).toEqual([0, 1, 2]);
+
+			var game2 = new ludorum_game_truco.ai.SubTruco("Hand", [2], [1]);
+			expect(game2.moves().Hand).toEqual([0]);
+
+			// Switch to foot
+			//expect(game2.moves().Foot).toEqual([0]);
 		});
 
 	});
