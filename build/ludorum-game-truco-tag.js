598497ea8d4c9c993c2f187f7efa0af81343ac74
Lucas Lois <lucasloisp@gmail.com>
2018-09-03 10:57:39 -0300

    Implementa función de envido

--- build/ludorum-game-truco-tag.js
+++ build/ludorum-game-truco-tag.js
@@ -63,7 +63,49 @@ function arrEq(a, b) {
       if (a[i] !== b[i]) return false;
     }
     return true;
-  }
+}
+
+function envidoTotal(cards) {
+    var a = envido(cards[0], cards[1]);
+    var b = envido(cards[0], cards[2]);
+    var c = envido(cards[1], cards[2]);
+
+    return Math.max(a, b, c);
+}
+
+/**
+ * Cada carta se representa con un entero del 0 al 39.
+ *
+ * Dado que hay 10 cartas de cada palo, y son 4 palos,
+ * el palo de una carta n es n mod 4.
+ *
+ * Su número es n // 4 + 1 (para hacerlo de 1 a 10).
+ *
+ * PALOS
+ * 0 - Espada
+ * 1 - Basto
+ * 2 - Oro
+ * 3 - Copa
+ *
+ * CARTAS
+ * 1..7 - 1..7
+ * 8 - 10
+ * 9 - 11
+ * 10 - 12
+ */
+
+function envido(a, b) {
+    var valor = (a % 4) == (b % 4) ? 20 : 0;
+    var numA = ~~(a / 4) + 1;
+    var numB = ~~(b / 4) + 1;
+    if (numA < 8) {
+        valor += numA;
+    }
+    if (numB < 8) {
+        valor += numB;
+    }
+    return valor;
+}
 
 /** # Truco base
 
