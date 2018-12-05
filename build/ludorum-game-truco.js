9fed007f51eb43db43ea5351eaebbfb9f7f55e05
Lucas Lois <lucasloisp@gmail.com>
2018-12-05 16:06:13 -0300

    Build 05/12

--- build/ludorum-game-truco.js
+++ build/ludorum-game-truco.js
@@ -359,9 +359,14 @@ var ChallengedTruco = exports.ai.ChallengedTruco = declare(SubTruco, {
 		SubTruco.call(this, table, cardsHand, cardsFoot);
 		this.globalScore = globalScore;
 
-		 // Collect up to now raised challenges
+		 // _Envido_ related
 		this.envidoStack = [];
+		this.envidoGoing = false; // TODO: Update envidoGoing when a challenge is raised/answered
+
+		// _Truco_ related
 		this.trucoStack = [];
+		this.trucoGoing = false; // TODO: Update trucoGoing when a challenge is raised/answered
+		this.canUpChallenge = null; // A player that can up the challenge later
 	},
 
 	/** The players' roles in a ChallengedTruco match are `"Hand"` (_Mano_) and `"Foot"` (_Pie_).
@@ -376,63 +381,31 @@ var ChallengedTruco = exports.ai.ChallengedTruco = declare(SubTruco, {
     challenge.
 	*/
 	moves: function moves() {
-		var moves = SubTruco.prototype.moves.call(this);
-		if (moves) {
-			var envidoChallenge = this.envidoStack[this.envidoStack.length - 1];
-			var trucoChallenge = this.trucoStack[this.trucoStack.length - 1];
-			if (envidoChallenge) {
-				moves[this.activePlayer()] = [
-					ChallengedTruco.CHALLENGES.Quiero,
-					ChallengedTruco.CHALLENGES.NoQuiero
-				];
+		var envidoChall = this.getEnvidoChallenge();
+		var trucoChall = this.getTrucoChallenge();
 
-				switch (envidoChallenge) {
-					case ChallengedTruco.CHALLENGES.Envido:
-						if (this.envidoStack.length === 1) {
-							Array.prototype.push.apply(moves[this.activePlayer()], [
-								ChallengedTruco.CHALLENGES.Envido
-							]);
-						}
-						Array.prototype.push.apply(moves[this.activePlayer()], [
-							ChallengedTruco.CHALLENGES.RealEnvido,
-							ChallengedTruco.CHALLENGES.FaltaEnvido,
-						]);
-						break;
-					case ChallengedTruco.CHALLENGES.RealEnvido:
-						Array.prototype.push.apply(moves[this.activePlayer()], [
-							ChallengedTruco.CHALLENGES.FaltaEnvido,
-						]);
-						break;
-				}
-			} else if (trucoChallenge) {
-				moves[this.activePlayer()] = [
-					ChallengedTruco.CHALLENGES.Quiero,
-					ChallengedTruco.CHALLENGES.NoQuiero
-				];
-				switch (lastRaisedChallenge) {
-					case ChallengedTruco.CHALLENGES.Truco:
-						Array.prototype.push.apply(moves[this.activePlayer()], [
-							ChallengedTruco.CHALLENGES.ReTruco
-						]);
-						break;
-					case ChallengedTruco.CHALLENGES.ReTruco:
-						Array.prototype.push.apply(moves[this.activePlayer()], [
-							ChallengedTruco.CHALLENGES.ValeCuatro
-						]);
-						break;
-				}
-				// TODO: Consider possible responses to the challenge
-			} else {
-				Array.prototype.push.apply(moves[this.activePlayer()], [
-					ChallengedTruco.CHALLENGES.Truco,
-				]);
-				if (this.table.length <= 1) {
-					Array.prototype.push.apply(moves[this.activePlayer()], [
-						ChallengedTruco.CHALLENGES.Envido,
-						ChallengedTruco.CHALLENGES.RealEnvido,
-						ChallengedTruco.CHALLENGES.FaltaEnvido
-					]);
-				}
+		var moves = SubTruco.prototype.moves.call(this);
+
+		if (this.envidoGoing) {
+			moves[this.activePlayer()] = [
+				ChallengedTruco.CHALLENGES.Quiero,
+				ChallengedTruco.CHALLENGES.NoQuiero
+			];
+			Array.prototype.push.apply(moves[this.activePlayer()], this.envidoResponses());
+		} else if (this.trucoGoing) {
+			moves[this.activePlayer()] = [
+				ChallengedTruco.CHALLENGES.Quiero,
+				ChallengedTruco.CHALLENGES.NoQuiero
+			];
+			Array.prototype.push.apply(moves[this.activePlayer()], this.trucoResponses());
+		} else {
+			// No challenges are in negotiation. Can raise new ones or play normally
+			if (this.table.length < 2) {
+				Array.prototype.push.apply(moves[this.activePlayer()], this.envidoResponses());
+			}
+
+			if (!trucoChall || this.canUpChallenge === this.activePlayer()) {
+				Array.prototype.push.apply(moves[this.activePlayer()], this.trucoResponses());
 			}
 		}
 		return moves;
@@ -450,12 +423,57 @@ var ChallengedTruco = exports.ai.ChallengedTruco = declare(SubTruco, {
 		var activePlayer = this.activePlayer();
 		var move = +moves[activePlayer];
 
-		if (move > 2) {
-			var that = update ? this : this.clone();
-			// TODO: Alter game state based on the raised challenge or response
-
-		} else {
+		if (move <= 2) {
 			return SubTruco.prototype.next.call(this, moves, haps, update);
+		} else {
+			var that = update ? this : this.clone();
+			var envidoChallenge = that.getEnvidoChallenge();
+			var trucoChall = that.getTrucoChallenge();
+
+			switch (move) {
+				case ChallengedTruco.CHALLENGES.Quiero:
+					if (envidoChallenge) {
+						var handEnvido = envidoTotal(that.cardsHand);
+						var footEnvido = envidoTotal(that.cardsFoot);
+						var envidoWinner = handEnvido >= footEnvido ? 'Hand' : 'Foot';
+						var envidoWinPoints = that.envidoStackWorth()[0];
+						that.envidoGoing = false;
+						// TODO: (meeting) How are the different ENVIDO scores published
+						// in Game.result()?
+					} else if (trucoChall) {
+						that.trucoGoing = false;
+						that.canUpChallenge =
+							trucoChall != ChallgruengedTruco.CHALLENGES.ValeCuatro ? activePlayer : null;
+					} else { /* IMPOSSIBLE */ }
+					break;
+
+				case ChallengedTruco.CHALLENGES.NoQuiero:
+					if (envidoChallenge) {
+						that.envidoGoing = false;
+						var envidoNotWantedPoints = that.envidoStackWorth()[1];
+						// TODO: Assign score to the challenging player, game continues
+					} else if (trucoChallenge) {
+						that.trucoGoing = false;
+						var challengerScore = that.trucoStackWorth() - 1;
+						// TODO: Assign score to the challenging player, game over
+					} else { /* IMPOSSIBLE */ }
+					break;
+
+				case ChallengedTruco.CHALLENGES.Truco:
+				case ChallengedTruco.CHALLENGES.ReTruco:
+				case ChallengedTruco.CHALLENGES.ValeCuatro:
+					that.canUpChallenge = null;
+					that.trucoGoing = true;
+					that.trucoStack.push(move);
+					break;
+
+				case ChallengedTruco.CHALLENGES.Envido:
+				case ChallengedTruco.CHALLENGES.RealEnvido:
+				case ChallengedTruco.CHALLENGES.FaltaEnvido:
+					that.envidoGoing = true;
+					that.envidoStack.push(move);
+					break;
+			}
 		}
 
 		return null;
@@ -476,6 +494,118 @@ var ChallengedTruco = exports.ai.ChallengedTruco = declare(SubTruco, {
 		return cloned_game;
 	},
 
+	trucoStackWorth: function trucoStackWorth() {
+		return this.trucoStack.length + 1;
+		// Truco: 2
+		// Truco, ReTruco: 3
+		// TRUCO; ReTruco, ValeCuatro: 4
+	},
+
+	getEnvidoChallenge: function() {
+		return this.envidoStack[this.envidoStack.length - 1];
+	},
+
+	getTrucoChallenge: function() {
+		return this.trucoStack[this.trucoStack.length - 1];
+	},
+
+	/**
+	 * When envido challenges are called their effect is cumulative. If an _Envido_ is answered to
+	 * with a _Real Envido_ then it is worth 5 (2 + 3) points. Not all sequences are allowed.
+	 * Below is the list of allowed _Envido_ type challenge chains, along with the points given to
+	 * the winner or when it is declined.
+	 *
+	 * E               2/1
+	 * RE              3/1
+	 * FE              x/1
+	 * E, E            4/2
+	 * E, RE           5/2
+	 * E, E, RE        7/4
+	 * E, E, RE, FE    x/7
+	 * E, FE           x/2
+	 * RE, FE          x/3
+	 * E, E, FE        x/4
+	 * E, RE, FE       x/5
+	 */
+	envidoStackWorth: function envidoStackWorth() {
+		if (this.envidoStack.length) {
+			return null;
+		}
+		var wanted = 1;
+		var notWanted = 0;
+		for (var i = 0; i < this.envidoStack.length; i++) {
+			notWanted = wanted;
+			switch (this.envidoStack[i]) {
+				case ChallengedTruco.CHALLENGES.Envido:
+					wanted += 2;
+					break;
+				case ChallengedTruco.CHALLENGES.RealEnvido:
+					notWanted = wanted;
+					wanted += 3;
+					break;
+				case ChallengedTruco.CHALLENGES.FaltaEnvido:
+					return [this.faltaEnvidoScore(), notWanted];
+			}
+		}
+		return [wanted, notWanted];
+	},
+
+	envidoResponses: function envidoResponses() {
+		var envidoChall = this.getEnvidoChallenge();
+		var possibleMoves = [];
+
+		if (!envidoChall || envidoChall < ChallengedTruco.CHALLENGES.FaltaEnvido) {
+			possibleMoves.push(ChallengedTruco.CHALLENGES.FaltaEnvido);
+		}
+
+		if (!envidoChall || envidoChall < ChallengedTruco.CHALLENGES.RealEnvido) {
+			possibleMoves.push(ChallengedTruco.CHALLENGES.RealEnvido);
+		}
+
+		if (!envidoChall || envidoChall < ChallengedTruco.CHALLENGES.Envido) {
+			possibleMoves.push(ChallengedTruco.CHALLENGES.Envido);
+		}
+
+		if (envidoChall === ChallengedTruco.CHALLENGES.Envido && this.envidoStack.length === 1) {
+			possibleMoves.push(ChallengedTruco.CHALLENGES.Envido);
+		}
+
+		return possibleMoves;
+	},
+
+	trucoResponses: function trucoResponses() {
+		var trucoChall = this.getTrucoChallenge();
+
+		switch (trucoChall) {
+			case ChallengedTruco.CHALLENGES.Truco:
+				return [ChallengedTruco.CHALLENGES.ReTruco];
+			case ChallengedTruco.CHALLENGES.ReTruco:
+				return [ChallengedTruco.CHALLENGES.ValeCuatro];
+			default:
+				return [ChallengedTruco.CHALLENGES.Truco];
+		}
+	},
+
+	/**
+	 * The _Falta Envido_ challenge depends on the global game status. If both players are in
+	 * _malas_ (up to 15 points each), the player that wins the _Falta Envido_ wins the  global
+	 * game. If at least one player is above 15 points this challenge is worth the amount of points
+	 * necessary for the winning player to win the global game.
+	 */
+	faltaEnvidoScore: function faltaEnvidoScore() {
+		var handGlobal = this.globalScore[0];
+		var footGlobal = this.globalScore[1];
+		if (handGlobal <= 15 && footGlobal <= 15) {
+			if (this.activePlayer() === 'Hand') {
+				return 30 - handGlobal;
+			} else {
+				return 30 - footGlobal;
+			}
+		} else {
+			return 30 - Math.max(handGlobal, footGlobal);
+		}
+	},
+
     /**
 
      */
@@ -490,28 +620,6 @@ var ChallengedTruco = exports.ai.ChallengedTruco = declare(SubTruco, {
 		'NoQuiero': 10
 	},
 
-	/**
-	 * The _Falta Envido_ challenge depends n the global game status. If both players are in _malas_
-	 * (up to 15 points each), the player that wins the _Falta Envido_ wins the global game. If at least
-	 * one player is above 15 points this challenge is worth the amount of points necessary for the winning
-	 * player to win the global game.
-	 *
-	 * When envido challenges are called their effect is cumulative. If an _Envido_ is answered to with a
-	 * _Real Envido_ then it is worth 5 (2 + 3) points. Not all sequences are allowed. Below is the list
-	 * of allowed _Envido_ type challenge chains, along with the points given to the winner or when it is declined.
-	 *
-	 * E               2/1
-	 * RE              3/1
-	 * FE              x/1
-	 * E, E            4/2
-	 * E, RE           5/2
-	 * E, E, RE        7/4
-	 * E, E, RE, FE    x/7
-	 * E, FE           x/2
-	 * RE, FE          x/3
-	 * E, E, FE        x/4
-	 * E, RE, FE       x/5
-	 */
 
 	/** Serialization is used in the `toString()` method, but it is also vital for sending the game
 	state across a network or the marshalling between the rendering thread and a webworker.
